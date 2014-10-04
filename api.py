0ba45876f08b287bfcffcc5f3a101e9c45f418dd
Eddy <klimslava@eddy-dev02.local>
2014-10-04 16:30:24 -0400

    added a map

--- api.py
+++ api.py
@@ -4,14 +4,19 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-    return app.send_static_file('public/client/index.html')
+    return app.send_static_file('index.html')
 
-import os
+
 @app.route('/js/<path:path>')
-def static_proxy(path):
+def static_proxy_js(path):
     # send_static_file will guess the correct MIME type
-    return app.send_static_file(os.path.join('public/client/js', path))
+    return app.send_static_file(os.path.join('js', path))
 
+
+@app.route('/bower_components/<path:path>')
+def static_proxy_bower(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(os.path.join('bower_components', path))
 
 if __name__ == "__main__":
     app.run()
